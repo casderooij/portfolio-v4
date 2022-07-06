da1966e7b29423d3fa7ed1b3742ac2504abba7f7
Cas de Rooij <hello@casderooij.nl>
2022-07-06 15:51:26 +0200

    feat: create project list page

---
+++ src/routes/projects/index.svelte
@@ -0,0 +1,25 @@
+<script context="module" lang="ts">
+	import type { LoadOutput } from '@sveltejs/kit';
+	export const load = async ({ fetch }): Promise<LoadOutput> => {
+		const response = await fetch(`/api/projects.json`);
+		const projects = await response.json();
+
+		return {
+			props: {
+				projects
+			}
+		};
+	};
+</script>
+
+<script lang="ts">
+	export let projects: any;
+</script>
+
+<ul>
+	{#each projects as project}
+		<li>
+			<a href="/projects/{project.meta.slug}">{project.meta.title}</a>
+		</li>
+	{/each}
+</ul>
